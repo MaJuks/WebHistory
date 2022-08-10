4fe7c5effe552c1c652e64d82b844a81612d8fe6
Maria Julia <mariajnascimento04@gmail.com>
2022-08-09 23:09:28 -0300

    brasilimperio

---
+++ js/brasil-imperio.js
@@ -0,0 +1,124 @@
+feather.replace()
+
+function btn_checked(){
+    if(document.getElementById("btn-midia").checked){
+        var xxx = document.getElementById('nav-hidden-midia').style.display = 'flex';
+    }
+    else{
+        var xxx = document.getElementById('nav-hidden-midia').style.display = 'none';
+    }
+}
+
+var count = 0;
+var btn_esp = document.getElementById("btn-especific2");
+var btn_esp2 = document.getElementById("btn-especific");
+
+var disp = document.getElementById("display");
+  
+btn_esp.onclick = function () {
+    count++;
+    disp.innerHTML = count;
+    if(count==0){
+        var wom1 = document.getElementById('woman1').style.display = 'flex';
+        var wom1_anim = document.getElementById('woman1').style.animation = 'fade-right 1s';
+
+        var wom2 = document.getElementById('woman2').style.display = 'none';
+        var wom3 = document.getElementById('woman3').style.display = 'none';
+        var wom4 = document.getElementById('woman4').style.display = 'none';
+    }
+    if(count==1 || count == (1*-3)){
+        var wom1 = document.getElementById('woman1').style.display = 'none';
+
+        var wom2 = document.getElementById('woman2').style.display = 'flex';
+        var wom2_anim = document.getElementById('woman2').style.animation = 'fade-right 1s';
+
+        var wom3 = document.getElementById('woman3').style.display = 'none';
+        var wom4 = document.getElementById('woman4').style.display = 'none';
+    }
+    if(count==2 || count == (1*-3)){
+        var wom1 = document.getElementById('woman1').style.display = 'none';
+        var wom2 = document.getElementById('woman2').style.display = 'none';
+
+        var wom3 = document.getElementById('woman3').style.display = 'flex';
+        var wom3_anim = document.getElementById('woman3').style.animation = 'fade-right 1s';
+
+        var wom4 = document.getElementById('woman4').style.display = 'none';
+    }
+    if(count==3 || count == (1*-1)){
+        var wom1 = document.getElementById('woman1').style.display = 'none';
+        var wom2 = document.getElementById('woman2').style.display = 'none';
+        var wom3 = document.getElementById('woman3').style.display = 'none';
+
+        var wom4 = document.getElementById('woman4').style.display = 'flex';
+        var wom4_anim = document.getElementById('woman4').style.animation = 'fade-right 1s';
+    }
+    if(count >= 4 || count == (1*-4)){
+        count = 0;
+        disp.innerHTML = count;
+        var wom1 = document.getElementById('woman1').style.display = 'flex';
+        var wom1_anim = document.getElementById('woman1').style.animation = 'fade-right 1s';
+
+        var wom2 = document.getElementById('woman2').style.display = 'none';
+        var wom3 = document.getElementById('woman3').style.display = 'none';
+        var wom4 = document.getElementById('woman4').style.display = 'none';
+    }
+}
+btn_esp2.onclick = function () {
+    count--;
+    disp.innerHTML = count;
+    if(count >= 4){
+        count = 0;
+        disp.innerHTML = count;
+
+    }
+    if(count==0){
+        var wom1 = document.getElementById('woman1').style.display = 'flex';
+        var wom1_anim = document.getElementById('woman1').style.animation = 'fade-left 1s';
+
+        var wom2 = document.getElementById('woman2').style.display = 'none';
+        var wom3 = document.getElementById('woman3').style.display = 'none';
+        var wom4 = document.getElementById('woman4').style.display = 'none';
+    }
+    // barbara alencar
+    if(count==1 || count == (1*-3)){
+        var wom1 = document.getElementById('woman1').style.display = 'none';
+
+        var wom2 = document.getElementById('woman2').style.display = 'flex';
+        var wom1_anim = document.getElementById('woman2').style.animation = 'fade-left 1s';
+
+        var wom3 = document.getElementById('woman3').style.display = 'none';
+        var qwom4 = document.getElementById('woman4').style.display = 'none';
+    }
+    if(count==2 || count == (1*-2)){
+        var wom1 = document.getElementById('woman1').style.display = 'none';
+        var wom2 = document.getElementById('woman2').style.display = 'none';
+
+        var wom3 = document.getElementById('woman3').style.display = 'flex';
+        var wom1_anim = document.getElementById('woman3').style.animation = 'fade-left 1s';
+
+        var wom4 = document.getElementById('woman4').style.display = 'none';
+    }
+    if(count==3 || count == (1*-1)){
+        var wom1 = document.getElementById('woman1').style.display = 'none';
+        var wom2 = document.getElementById('woman2').style.display = 'none';
+        var wom3 = document.getElementById('woman3').style.display = 'none';
+
+        var wom4 = document.getElementById('woman4').style.display = 'flex';
+        var wom1_anim = document.getElementById('woman4').style.animation = 'fade-left 1s';
+    }
+    if(count >= 4 || count == (1*-4)){
+        count = 0;
+        disp.innerHTML = count;
+        var wom1 = document.getElementById('woman1').style.display = 'flex';
+        var wom1_anim = document.getElementById('woman1').style.animation = 'fade-left 1s';
+
+        var wom2 = document.getElementById('woman2').style.display = 'none';
+        var wom3 = document.getElementById('woman3').style.display = 'none';
+        var wom4 = document.getElementById('woman4').style.display = 'none';
+    }
+}
+
+AOS.init();
+
+        
+
