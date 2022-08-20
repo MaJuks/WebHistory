648e48a7ba0b59796bc868a86980ed7424ccfca2
Maria Julia <mariajnascimento04@gmail.com>
2022-08-19 22:53:13 -0300

    nova-republica

---
+++ js/nova-republica.js
@@ -0,0 +1,57 @@
+feather.replace()
+
+var count = 0;
+var btn_right = document.getElementById("btn-right");
+var btn_left = document.getElementById("btn-left");
+var disp = document.getElementById("display");
+  
+btn_right.onclick = function () {
+    count++;
+    disp.innerHTML = count;
+    if(count==0){
+        var wom1 = document.getElementById('woman1').style.display = 'flex';
+        var wom1_anim = document.getElementById('woman1').style.animation = 'fade-right 1s';
+
+        var wom2 = document.getElementById('woman2').style.display = 'none';
+    }
+    if(count==1 || count == (1*-1)){
+        var wom1 = document.getElementById('woman1').style.display = 'none';
+
+        var wom2 = document.getElementById('woman2').style.display = 'flex';
+        var wom2_anim = document.getElementById('woman2').style.animation = 'fade-right 1s';
+    }
+    if(count >= 2 || count == (1*-2)){
+        count = 0;
+        disp.innerHTML = count;
+        var wom1 = document.getElementById('woman1').style.display = 'flex';
+        var wom1_anim = document.getElementById('woman1').style.animation = 'fade-right 1s';
+
+        var wom2 = document.getElementById('woman2').style.display = 'none';
+    }
+}
+btn_left.onclick = function () {
+    count--;
+    disp.innerHTML = count;
+    if(count==0){
+        var wom1 = document.getElementById('woman1').style.display = 'flex';
+        var wom1_anim = document.getElementById('woman1').style.animation = 'fade-right 1s';
+
+        var wom2 = document.getElementById('woman2').style.display = 'none';
+    }
+    if(count==1 || count == (1*-1)){
+        var wom1 = document.getElementById('woman1').style.display = 'none';
+
+        var wom2 = document.getElementById('woman2').style.display = 'flex';
+        var wom2_anim = document.getElementById('woman2').style.animation = 'fade-right 1s';
+    }
+    if(count >= 2 || count == (1*-2)){
+        count = 0;
+        disp.innerHTML = count;
+        var wom1 = document.getElementById('woman1').style.display = 'flex';
+        var wom1_anim = document.getElementById('woman1').style.animation = 'fade-right 1s';
+
+        var wom2 = document.getElementById('woman2').style.display = 'none';
+    }
+}
+
+AOS.init();
