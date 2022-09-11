d96da93a6da1facfd1edc3cba91003c2330af8b0
Maria Julia <mariajnascimento04@gmail.com>
2022-09-11 17:03:56 -0300

    modal-container test

--- js/brasil-colonia.js
+++ js/brasil-colonia.js
@@ -239,6 +239,67 @@ btn_left.onclick = function () {
     }
 }
 
+// modal
+function iniciaModal(modalID){
+    const modal = document.getElementById(modalID);
+    if(modal){ 
+        modal.classList.add('mostrar');
+        modal.addEventListener("click", (e) =>{
+        if (e.target.id == modalID || e.target.className == "fechar"){
+            modal.classList.remove("mostrar")
+        }
+        });
+    }
+}
+// img-native
+var img_nat1 = document.getElementById('img-native1');
+img_nat1.addEventListener('click', () => {
+    iniciaModal('modal-id-native1')});
+var img_nat2 = document.getElementById('img-native2');
+img_nat2.addEventListener('click', () => {
+    iniciaModal('modal-id-native2')});
+var img_nat3 = document.getElementById('img-native3');
+img_nat3.addEventListener('click', () => {
+    iniciaModal('modal-id-native3')});
+
+// img-black
+// bloco1
+var img_blck1 = document.getElementById('img-black1');
+img_blck1.addEventListener('click', () => {
+    iniciaModal('modal-id-black1')});
+var img_blck2 = document.getElementById('img-black2');
+img_blck2.addEventListener('click', () => {
+    iniciaModal('modal-id-black2')});
+// bloco2
+var img_blck3 = document.getElementById('img-black3');
+img_blck3.addEventListener('click', () => {
+    iniciaModal('modal-id-black3')});
+var img_blck4 = document.getElementById('img-black4');
+img_blck4.addEventListener('click', () => {
+    iniciaModal('modal-id-black4')});
+var img_blck5 = document.getElementById('img-black5');
+img_blck5.addEventListener('click', () => {
+    iniciaModal('modal-id-black5')});
+
+// img-white
+// bloco1
+var img_white1 = document.getElementById('img-white1');
+img_white1.addEventListener('click', () => {
+    iniciaModal('modal-id-white1')});
+// bloco2
+var img_white2 = document.getElementById('img-white2');
+img_white2.addEventListener('click', () => {
+    iniciaModal('modal-id-white2')});
+var img_white3 = document.getElementById('img-white3');
+img_white3.addEventListener('click', () => {
+    iniciaModal('modal-id-white3')});
+var img_white4 = document.getElementById('img-white4');
+img_white4.addEventListener('click', () => {
+    iniciaModal('modal-id-white4')});
+
+
+
+
 AOS.init();
 
         
